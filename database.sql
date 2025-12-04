83b2b834f1a0b7506b7de687729e3b4105719f82
AISHWARYA MOUNDEKAR <123377001+aishwarya-moundekar@users.noreply.github.com>
2025-12-04 15:41:08 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS employee_leave_system;
+USE employee_leave_system;
+
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    total_leave_balance INT DEFAULT 20
+);
+
+CREATE TABLE IF NOT EXISTS leave_requests (
+    request_id INT AUTO_INCREMENT PRIMARY KEY,
+    employee_id INT,
+    leave_type VARCHAR(20),
+    start_date DATE,
+    end_date DATE,
+    days INT,
+    status ENUM('Pending','Approved','Rejected') DEFAULT 'Pending',
+    applied_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
